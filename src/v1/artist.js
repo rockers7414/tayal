52c42613d4fd2a28f3788e242058678c804a65ad
RCK <rockers7414@gmail.com>
2017-10-13 22:17:47 +0800

    Add status code and validate the input

--- src/v1/artist.js
+++ src/v1/artist.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 const Response = require('../objects/response');
+const Err = require('../objects/error');
+
 const Artist = require('../modules/artist');
 const Album = require('../modules/album');
 
@@ -8,48 +10,69 @@ router.get('/', (req, res) => {
   const index = req.query.index ? parseInt(req.query.index) : 0;
   const offset = req.query.offset ? parseInt(req.query.offset) : 50;
   Artist.getArtists(index, offset).then(page => {
-    res.send(new Response.Collection(page.data, page.index, page.offset, page.total));
+    res.status(200).send(new Response.Collection(page.data, page.index, page.offset, page.total));
   });
 });
 
 router.post('/', (req, res) => {
+  if (!req.body.name || req.body.name == '') {
+    res.status(400)
+      .send(new Response.Error(new Err.InvalidParam(['name is required'])));
+  }
   new Artist(req.body.name).save().then(artist => {
-    res.send(new Response.Data(artist));
+    res.status(200).send(new Response.Data(artist));
   });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id(\\w{12})', (req, res) => {
   Artist.getArtist(req.params.id).then(artist => {
-    res.send(new Response.Data(artist));
+    res.status(200).send(new Response.Data(artist));
   });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id(\\w{12})', (req, res) => {
+  if (!req.body.name || req.body.name == '') {
+    res.status(400)
+      .send(new Response.Error(new Err.InvalidParam(['name is required'])));
+  }
   Artist.getArtist(req.params.id)
     .then(artist => {
       artist.name = req.body.name;
       artist.albums = req.body.albums || [];
       return artist.save();
     }).then(artist => {
-      res.send(new Response.Data(artist));
+      res.status(200).send(new Response.Data(artist));
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id(\\w{12})', (req, res) => {
   Artist.deleteArtist(req.params.id)
     .then(result => res.send(new Response.Data(result)))
-    .catch(e => res.send(new Response.Error(e)));
+    .catch(e => {
+      if (e instanceof Err.UnremovableError) {
+        res.status(400);
+      }
+      res.send(new Response.Error(e));
+    });
 });
 
-router.post('/:id/albums', (req, res) => {
+router.post('/:id(\\w{12})/albums', (req, res) => {
+  if (!req.body.name || req.body.name == '') {
+    res.status(400)
+      .send(new Response.Error(new Err.InvalidParam(['name is required'])));
+  }
   Artist.getArtist(req.params.id)
     .then(artist => {
       new Album(req.body.name, artist.toSimple()).save()
         .then(album => {
           artist.albums.push(album.toSimple());
-          artist.save().then(artist => res.send(new Response.Data(artist)));
+          artist.save().then(artist => res.status(200).send(new Response.Data(artist)));
         });
     });
+});
+
+router.get('*', (req, res) => {
+  res.status(404).send(new Response.Error(new Err.ResourceNotFound()));
 });
 
 module.exports = router;
