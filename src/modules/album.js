e7d89f21de6dbec6b0a616a4d9ea1c5f4496fcf9
AbnerLin <shaoyang0805@gmail.com>
2017-10-17 21:45:47 +0800

    Update indent.

--- src/modules/album.js
+++ src/modules/album.js
@@ -5,91 +5,91 @@ const Page = require('../objects/page');
 const Error = require('../objects/error');
 
 class Album {
-    static getAlbums(index = 0, offset = 50) {
-        return Database.getCollection('albums')
-            .then(collection => {
-                return new Promise((resolve, reject) => {
-                    collection.find().skip(index).limit(offset).toArray((err, data) => {
-                        if (err) {
-                            reject(err);
-                        }
-
-                        const albums = data.map(data => {
-                            const album = new Album(data.name, data.artist, data.tracks, data.images);
-                            album._id = data._id;
-                            return album;
-                        });
-
-                        resolve(new Page(index, offset, albums, data.length));
-                    })
-                });
-            });
-    }
-
-    static getAlbum(id) {
-        return Database.getCollection('albums')
-            .then(collection => {
-                return collection.findOne({ _id: new ObjectID(id) });
-            }).then(data => {
-                const album = new Album(data.name, data.artist, data.tracks, data.images);
-                album._id = data._id;
-                return album;
-            });
-    }
-
-    static deleteAlbum(id) {
-        return Album.getAlbum(id).then(album => {
-            if (album.tracks.lengh > 0) {
-                throw new Error.UnremoveableError('has related tracks');
+  static getAlbums(index = 0, offset = 50) {
+    return Database.getCollection('albums')
+      .then(collection => {
+        return new Promise((resolve, reject) => {
+          collection.find().skip(index).limit(offset).toArray((err, data) => {
+            if (err) {
+              reject(err);
             }
 
-            return Database.getCollection('albums')
-                .then(collection => {
-                    return collection.deleteOne({ _id: new ObjectID(id) });
-                }).then(result => {
-                    return result.deleteCount == 1;
-                });
-        });
-    }
-
-    constructor(name, artist, tracks = [], images = []) {
-        this.name = name;
-        this.artist = artist;
-        this.tracks = tracks;
-        this.images = images;
-    }
-
-    save() {
-        return Database.getCollection('albums')
-            .then(collection => {
-                return new Promise((resolve, reject) => {
-                    collection.updateOne({ _id: new ObjectID(this._id) },
-                        this, { upsert: true },
-                        (err, res) => {
-                            if (err) {
-                                reject(err);
-                            }
-
-                            if (!res.matchedCount && !res.upsertedCount) {
-                                resolve(null);
-                            }
-
-                            if (!this._id) {
-                                this._id = res.result.upserted[0]._id.toString();
-                            }
-
-                            resolve(this);
-                        });
-                });
+            const albums = data.map(data => {
+              const album = new Album(data.name, data.artist, data.tracks, data.images);
+              album._id = data._id;
+              return album;
             });
-    }
 
-    toSimple() {
-        return {
-            _id: this._id,
-            name: this.name
-        };
-    }
+            resolve(new Page(index, offset, albums, data.length));
+          })
+        });
+      });
+  }
+
+  static getAlbum(id) {
+    return Database.getCollection('albums')
+      .then(collection => {
+        return collection.findOne({ _id: new ObjectID(id) });
+      }).then(data => {
+        const album = new Album(data.name, data.artist, data.tracks, data.images);
+        album._id = data._id;
+        return album;
+      });
+  }
+
+  static deleteAlbum(id) {
+    return Album.getAlbum(id).then(album => {
+      if (album.tracks.lengh > 0) {
+        throw new Error.UnremoveableError('has related tracks');
+      }
+
+      return Database.getCollection('albums')
+        .then(collection => {
+          return collection.deleteOne({ _id: new ObjectID(id) });
+        }).then(result => {
+          return result.deleteCount == 1;
+        });
+    });
+  }
+
+  constructor(name, artist, tracks = [], images = []) {
+    this.name = name;
+    this.artist = artist;
+    this.tracks = tracks;
+    this.images = images;
+  }
+
+  save() {
+    return Database.getCollection('albums')
+      .then(collection => {
+        return new Promise((resolve, reject) => {
+          collection.updateOne({ _id: new ObjectID(this._id) },
+            this, { upsert: true },
+            (err, res) => {
+              if (err) {
+                reject(err);
+              }
+
+              if (!res.matchedCount && !res.upsertedCount) {
+                resolve(null);
+              }
+
+              if (!this._id) {
+                this._id = res.result.upserted[0]._id.toString();
+              }
+
+              resolve(this);
+            });
+        });
+      });
+  }
+
+  toSimple() {
+    return {
+      _id: this._id,
+      name: this.name
+    };
+  }
 }
 
 module.exports = Album;
--- src/v1/artist.js
+++ src/v1/artist.js
@@ -7,73 +7,73 @@ const Artist = require('../modules/artist');
 const Album = require('../modules/album');
 
 router.get('/', (req, res) => {
-    const index = req.query.index ? parseInt(req.query.index) : 0;
-    const offset = req.query.offset ? parseInt(req.query.offset) : 50;
-    Artist.getArtists(index, offset).then(page => {
-        res.status(200).send(new Response.Collection(page.data, page.index, page.offset, page.total));
-    });
+  const index = req.query.index ? parseInt(req.query.index) : 0;
+  const offset = req.query.offset ? parseInt(req.query.offset) : 50;
+  Artist.getArtists(index, offset).then(page => {
+    res.status(200).send(new Response.Collection(page.data, page.index, page.offset, page.total));
+  });
 });
 
 router.post('/', (req, res) => {
-    if (!req.body.name || req.body.name == '') {
-        res.status(400)
-            .send(new Response.Error(new Err.InvalidParam(['name is required'])));
-    }
-    new Artist(req.body.name).save().then(artist => {
-        res.status(200).send(new Response.Data(artist));
-    });
+  if (!req.body.name || req.body.name == '') {
+    res.status(400)
+      .send(new Response.Error(new Err.InvalidParam(['name is required'])));
+  }
+  new Artist(req.body.name).save().then(artist => {
+    res.status(200).send(new Response.Data(artist));
+  });
 });
 
 router.get('/:id(\\w{12})', (req, res) => {
-    Artist.getArtist(req.params.id).then(artist => {
-        res.status(200).send(new Response.Data(artist));
-    });
+  Artist.getArtist(req.params.id).then(artist => {
+    res.status(200).send(new Response.Data(artist));
+  });
 });
 
 router.put('/:id(\\w{12})', (req, res) => {
-    if (!req.body.name || req.body.name == '') {
-        res.status(400)
-            .send(new Response.Error(new Err.InvalidParam(['name is required'])));
-    }
-    Artist.getArtist(req.params.id)
-        .then(artist => {
-            artist.name = req.body.name;
-            artist.albums = req.body.albums || [];
-            return artist.save();
-        }).then(artist => {
-            res.status(200).send(new Response.Data(artist));
-        });
+  if (!req.body.name || req.body.name == '') {
+    res.status(400)
+      .send(new Response.Error(new Err.InvalidParam(['name is required'])));
+  }
+  Artist.getArtist(req.params.id)
+    .then(artist => {
+      artist.name = req.body.name;
+      artist.albums = req.body.albums || [];
+      return artist.save();
+    }).then(artist => {
+      res.status(200).send(new Response.Data(artist));
+    });
 });
 
 router.delete('/:id(\\w{12})', (req, res) => {
-    Artist.deleteArtist(req.params.id)
-        .then(result => res.send(new Response.Data(result)))
-        .catch(e => {
-            if (e instanceof Err.UnremovableError) {
-                res.status(400);
-            }
-            res.send(new Response.Error(e));
-        });
+  Artist.deleteArtist(req.params.id)
+    .then(result => res.send(new Response.Data(result)))
+    .catch(e => {
+      if (e instanceof Err.UnremovableError) {
+        res.status(400);
+      }
+      res.send(new Response.Error(e));
+    });
 });
 
 router.post('/:id(\\w{24})/albums', (req, res) => {
-    if (!req.body.name || req.body.name == '') {
-        res.status(400)
-            .send(new Response.Error(new Err.InvalidParam(['name is required'])));
-    }
-    Artist.getArtist(req.params.id)
-        .then(artist => {
-            new Album(req.body.name, artist.toSimple()).save()
-                .then(album => {
-                    artist.albums.push(album.toSimple());
-                    artist.save().then(artist => res.status(200).send(new Response.Data(artist)));
-                });
-
+  if (!req.body.name || req.body.name == '') {
+    res.status(400)
+      .send(new Response.Error(new Err.InvalidParam(['name is required'])));
+  }
+  Artist.getArtist(req.params.id)
+    .then(artist => {
+      new Album(req.body.name, artist.toSimple()).save()
+        .then(album => {
+          artist.albums.push(album.toSimple());
+          artist.save().then(artist => res.status(200).send(new Response.Data(artist)));
         });
+
+    });
 });
 
 router.get('*', (req, res) => {
-    res.status(404).send(new Response.Error(new Err.ResourceNotFound()));
+  res.status(404).send(new Response.Error(new Err.ResourceNotFound()));
 });
 
 module.exports = router;
