201f78d4331631860fbbb85b7e9b4cdbde49a5e0
AbnerLin <shaoyang0805@gmail.com>
2017-11-13 00:28:51 +0800

    Add album UT(not done yet.)

--- src/server.js
+++ src/server.js
@@ -21,3 +21,5 @@ app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port, () => console.log(`API running on localhost:${port}`));
+
+module.exports = server;
---
+++ test/albumTest.js
@@ -0,0 +1,60 @@
+const assert = require('assert');
+const should = require('should');
+
+const app = require('../src/server.js')
+const Album = require('../src/modules/album')
+const Track = require('../src/modules/Track')
+const request = require('supertest');
+const express = require('express');
+
+
+describe('Album Test', () => {
+
+  var album = null;
+
+  before(() => {
+    // console.log('Server');
+  });
+
+  after(() => {
+    app.close();
+    process.exit();
+  });
+
+  beforeEach(() => {
+    return new Album('Perfect Strangers', null, [], null).save().then(_album => {
+      album = _album;
+    });
+  });
+
+  afterEach(() => {
+    return Album.deleteAlbum(album._id);
+  });
+
+  describe('#find(id)', () => {
+    it('response with matching records', () => {
+      return Album.getAlbum(album._id).then(_album => {
+        assert.equal(_album._id, album._id);
+      });
+    });
+  });
+
+  describe('#queryAlbum()', () => {
+    it('should respond with JSON array', (done) => {
+      request(app)
+        .get('/api/v1/albums/')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) {
+          	console.log(err);
+          	return done(err);
+          }
+
+          res.body.data.should.be.instanceof(Array);
+          done();
+        });
+    });
+  });
+
+});
